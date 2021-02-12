dde95663572f12f30fe39a2667ba07c1e519ba9b
Berkius01 <58213980+Berkius01@users.noreply.github.com>
2021-02-13 02:50:07 +0300

    Add files via upload

---
+++ MyDict.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koleksiyonlar
+{
+    class MyDict<T>
+    {
+        T[] items;
+        //consturctor
+        public MyDict()
+        {
+            items = new T[0];
+        }
+        public void Add(T item) 
+        {
+            T[] temp = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                items[i] = temp[i];
+            }
+            items[items.Length-1] = item;
+        }
+
+        //public int Length
+        //{
+        //    get { return items.Length; }
+        //}
+        
+        //public T[] Items
+        //{
+        //    get { return items; }
+        //}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //dizi tanımlama c ve c++ ile aynı
+            //string[] isimler = new string[] {"engin","murat","kerem","halil"};
+            // isimler[0]=engin
+
+            
+            List<string> isimler2 = new List<string>();
+            isimler2.Add("sucuk");
+            
+            List<string> isimler2 = new List<string> {"sucuk","salam","sosig"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+
+            isimler2.Add("pastırma");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+            */ 
+            //neden böyle yaptık çünkü çalışacağı veri tipini belirledik
+            MyDict<string> isimler = new MyDict<string>();
+            isimler.Add("ankara");
+            isimler.Add("adana");
+            isimler.Add("istanbul");
+            isimler.Add("izmir");
+
+           //foreach (var isim in isimler.Items)
+           // {
+           //     Console.WriteLine(isim);
+           // }
+
+
+        }
+    }
+}
